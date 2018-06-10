8a877af927d242c8e6dc115c3249c4dfd0906df0
KimMolang <bluevill04@gmail.com>
2018-06-10 23:59:24 +0900

    Rotate

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -7,7 +7,7 @@ public class Controller : MonoBehaviour {
     public float mouseSensitivity = 2.0f;
     public float speed = 10.0f;
 
-    private Vector3 m_vRatationRadian   = new Vector3(0.0f, 0.0f, 1.0f);
+    private Vector3 m_vRatationRadian   = new Vector3(0.0f, 0.0f, 0.0f);
 
     // Use this for initialization
     void Start () {
@@ -32,17 +32,18 @@ public class Controller : MonoBehaviour {
         float fHorizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;// * Time.smoothDeltaTime;
         float fVerticalRotation     = -1 * (Input.GetAxis("Mouse Y") * mouseSensitivity);
 
-        //m_vRatationRadian.y += fHorizontalRotation * Time.smoothDeltaTime;
-        //m_vRatationRadian.x += fVerticalRotation * Time.smoothDeltaTime;
+        m_vRatationRadian.y += fHorizontalRotation;// * Time.smoothDeltaTime;
+        m_vRatationRadian.x += fVerticalRotation;// * Time.smoothDeltaTime;
 
         //if (m_vRatationRadian.x > MAX_DIR_X)
         //    m_vRatationRadian.x = MAX_DIR_X;
         //else if (m_vRatationRadian.x < MIN_DIR_X)
         //    m_vRatationRadian.x = MIN_DIR_X;
 
-        Debug.Log(Input.GetAxis("Mouse X"));
-        //transform.forward;
-        transform.Rotate(fVerticalRotation, fHorizontalRotation, 0.0f);
+        transform.rotation = Quaternion.Euler
+            ( transform.rotation.x + m_vRatationRadian.x
+            , transform.rotation.y + m_vRatationRadian.y
+            , 0.0f );
     }
 
     private void UpdateMove()
