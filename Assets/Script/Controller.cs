e7c949fe2a1b5b02c7fe50d974e5bce106615faf
KimMolang <bluevill04@gmail.com>
2018-06-11 00:18:42 +0900

    Complete basic movement!!

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
 
-    public float mouseSensitivity = 2.0f;
-    public float speed = 10.0f;
+    [Header("Ratate And Move")]
+    [SerializeField]
+    [Range(1.0f, 10.0f)]
+    private float m_fMouseSensitivity = 5.0f;
+    [SerializeField]
+    [Range(0.0f, 80.0f)]
+    private float m_fSpeed = 20.0f;
 
     private Vector3 m_vRatationRadian   = new Vector3(0.0f, 0.0f, 0.0f);
 
@@ -26,31 +31,33 @@ public class Controller : MonoBehaviour {
 
     private void UpdateRotate()
     {
-        const float MAX_DIR_X = 1.0f;
-        const float MIN_DIR_X = -1.2f;
+        const float MAX_DIR_X = 70.0f;
+        const float MIN_DIR_X = -70.0f;
 
-        float fHorizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;// * Time.smoothDeltaTime;
-        float fVerticalRotation     = -1 * (Input.GetAxis("Mouse Y") * mouseSensitivity);
+        float fHorizontalRotation = Input.GetAxis("Mouse X") * m_fMouseSensitivity;
+        float fVerticalRotation     = -1 * (Input.GetAxis("Mouse Y") * m_fMouseSensitivity);
 
         m_vRatationRadian.y += fHorizontalRotation;// * Time.smoothDeltaTime;
         m_vRatationRadian.x += fVerticalRotation;// * Time.smoothDeltaTime;
 
-        //if (m_vRatationRadian.x > MAX_DIR_X)
-        //    m_vRatationRadian.x = MAX_DIR_X;
-        //else if (m_vRatationRadian.x < MIN_DIR_X)
-        //    m_vRatationRadian.x = MIN_DIR_X;
+        if (m_vRatationRadian.x > MAX_DIR_X)
+            m_vRatationRadian.x = MAX_DIR_X;
+        else if (m_vRatationRadian.x < MIN_DIR_X)
+            m_vRatationRadian.x = MIN_DIR_X;
 
         transform.rotation = Quaternion.Euler
             ( transform.rotation.x + m_vRatationRadian.x
             , transform.rotation.y + m_vRatationRadian.y
             , 0.0f );
+
+        //Debug.Log(m_vRatationRadian.x);
     }
 
     private void UpdateMove()
     {
         float fVerticalMovement     = Input.GetAxisRaw("Vertical");
 
-        transform.position += transform.forward * fVerticalMovement * speed * Time.smoothDeltaTime;
+        transform.position += transform.forward * fVerticalMovement * m_fSpeed * Time.smoothDeltaTime;
     }
 
     void FixedUpdate()
