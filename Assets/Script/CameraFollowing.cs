bf2700481c08bc19baea78b247134cb005607c17
KimMolang <bluevill04@gmail.com>
2018-06-10 23:39:17 +0900

    First Commit And Complete 'Update move' function

---
+++ Assets/Script/CameraFollowing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour {
+
+    // (수정)
+    // enum value 카메라 상태
+    //public Vector3 CamRotation = new Vector3(45.0f, -45.0f, 0.0f);
+
+    // (수정) 이건 나중에 메이플 스토리 카메라 처럼, 길이 판정 할 것임.
+    public Vector3 CamPosition = new Vector3(0, 5, -5);
+
+    // (수정) 크리티컬 맞으면 카메라 흔들리는 효과
+
+    private Transform targetTranstorm = null;
+
+    public void SetTargetTransfrom( GameObject _target )
+    {
+        targetTranstorm = _target.transform;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate()
+    {
+        // (수정) 이 리턴문은 null 객체를 만들어서 없앨 것이다.
+        if (targetTranstorm == null)
+            return;
+
+        targetTranstorm.position = targetTranstorm.position + CamPosition;
+        //targetTranstorm.localRotation = Quaternion.Euler(CamRotation);
+    }
+}
---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public float mouseSensitivity = 2.0f;
+    public float speed = 10.0f;
+
+    private Vector3 m_vRatationRadian   = new Vector3(0.0f, 0.0f, 1.0f);
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateRotate();
+        UpdateMove();
+        
+        
+        
+    }
+
+    private void UpdateRotate()
+    {
+        const float MAX_DIR_X = 1.0f;
+        const float MIN_DIR_X = -1.2f;
+
+        float fHorizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;// * Time.smoothDeltaTime;
+        float fVerticalRotation     = -1 * (Input.GetAxis("Mouse Y") * mouseSensitivity);
+
+        //m_vRatationRadian.y += fHorizontalRotation * Time.smoothDeltaTime;
+        //m_vRatationRadian.x += fVerticalRotation * Time.smoothDeltaTime;
+
+        //if (m_vRatationRadian.x > MAX_DIR_X)
+        //    m_vRatationRadian.x = MAX_DIR_X;
+        //else if (m_vRatationRadian.x < MIN_DIR_X)
+        //    m_vRatationRadian.x = MIN_DIR_X;
+
+        Debug.Log(Input.GetAxis("Mouse X"));
+        //transform.forward;
+        transform.Rotate(fVerticalRotation, fHorizontalRotation, 0.0f);
+    }
+
+    private void UpdateMove()
+    {
+        float fVerticalMovement     = Input.GetAxisRaw("Vertical");
+
+        transform.position += transform.forward * fVerticalMovement * speed * Time.smoothDeltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
