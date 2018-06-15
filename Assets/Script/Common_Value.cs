7d5b697d05ed8a33f40c0a1086b6475ef6ba70da
KimMolang <bluevill04@gmail.com>
2018-06-16 01:47:29 +0900

    코드가 정리가 안 됨..

---
+++ Assets/Script/Common_Value.cs
@@ -0,0 +1,17 @@
+﻿
+static class VALUE_GAME_PLAY
+{
+    public const int MAX_HP_BASIC = 500;
+
+    public const float MAX_UNCONTROLLABLE_TIME_FALLING = 3.0f;
+}
+
+namespace Test
+{
+    public enum EOMG
+    {
+        NONE    = 0,
+
+    }
+
+}
